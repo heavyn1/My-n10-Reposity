379874967467ed1d7911319a0932e1bf58f001f1
heavyn1 <bunedusupov968@gmail.com>
2024-04-18 15:12:12 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// let numberOne = parseInt(prompt("enter a ="));
+// let textOne = prompt("Enter your text=");
+// let b;
+// let boolen1= true;
+// let bollen2= false;
+// let bigTest = 565626562312156463232163215465n;
+// alert("numburOne type =" + typeof (numburOne) + "\nextOne="
+//  + typeof (textOne) + "\nb type = "  + b + "\nboolen1 type = " + typeof (boolen1) + "\nboolen2")
+
+ 
+
+
+
+let firstText = prompt("Enter text =");
+let secondNumber = Number(prompt ("Enter your number ="));
+let thirdBoolin = Boolean=(prompt ("Enter your boole ="));
+alert("Siz to'g'ri qiymatlarni kiritganingizda ishonchingizni komilmi?")
+alert("type of firstText = " + typeof(firstText) + "\n  type of secondNumber =  " + typeof(secondNumber) + "\n type of thirdBoolen =" + typeof(thirdBoolen))
